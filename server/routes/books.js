ddee661e6a3f4cddb4b4daa5f922b687b13a86ac
samar <samoramahmoud22@gmail.com>
2020-05-03 00:24:44 +0200

    routes for books page

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,16 +1,23 @@
 
 const express = require('express')
 const router = express.Router()
+var path = require("path");
 
 
-
-router.get("/", async (request, response) => {});
-
-router.get("/javascript/admin.js", function (req, res) {
+router.get("/javascript/books.js", function (req, res) {
   res.set("Content-Type", "text/javascript");
-  res.sendFile(path.resolve("../client/_site/javascript/admin.js"));
+  res.sendFile(path.resolve("../client/_site/javascript/books.js"));
 });
 
+router.get("/stylecheets/books.css", function (req, res) {
+  res.set("Content-Type", "text/css");
+  res.sendFile(path.resolve("../client/_site/stylecheets/books.css"));
+});
+
+router.get("/", function (req, res) {
+  res.set("Content-Type", "text/html");
+  res.sendFile(path.resolve("../client/_site/html/books/books.html"));
+});
 
 
 
