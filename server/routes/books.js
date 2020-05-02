148d8dfea0e4cd53fdc4cbec9744910392e38aed
samar <samoramahmoud22@gmail.com>
2020-05-02 20:06:15 +0200

    initial books.js and books.html

--- server/routes/books.js
+++ server/routes/books.js
@@ -4,6 +4,12 @@ const router = express.Router()
 
 
 
+router.get("/", async (request, response) => {});
+
+router.get("/javascript/admin.js", function (req, res) {
+  res.set("Content-Type", "text/javascript");
+  res.sendFile(path.resolve("../client/_site/javascript/admin.js"));
+});
 
 
 
