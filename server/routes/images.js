250d75021abf9a8584bf1da333b892525dd37da5
Atef Hares <atef_moh@sci.asu.edu.eg>
2020-05-09 22:57:06 +0200

    routes for reviews, rates, images

--- server/routes/images.js
+++ server/routes/images.js
@@ -3,10 +3,15 @@ var path = require("path");
 const router = express.Router();
 
 router.get("/books/:id", function (req, res) {
-  console.log("heeeeeeeeeeeeere");
+  res.sendFile(path.resolve("../server/public/books/" + req.params.id));
+});
 
-  res.set("Content-Type", "image/jpg");
-  res.sendFile(path.resolve("../public/default_profile_pic.jpg"));
+router.get("/authors/:id", function (req, res) {
+  res.sendFile(path.resolve("../server/public/authors/" + req.params.id));
+});
+
+router.get("/users/:id", function (req, res) {
+  res.sendFile(path.resolve("../server/public/users/" + req.params.id));
 });
 
 module.exports = router;
--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -1,7 +1,124 @@
 const express = require("express");
 var path = require("path");
 const router = express.Router();
+const reviewsModel = require("../models/book_review");
+const booksModel = require("../models/Book");
+const usersModel = require("../models/User");
 
+// ==========================================================================
 
+router.get("/", async (request, response) => {
+  try {
+    const reviews = await reviewsModel.find({});
+    response.json(reviews);
+  } catch (error) {
+    console.log(error);
+    response.status(400).send();
+  }
+});
+
+router.get("/:id", async (request, response) => {
+  try {
+    const review = await reviewsModel.findById(request.params.id);
+    response.json(review);
+  } catch (error) {
+    console.log(error);
+    response.status(400).send();
+  }
+});
+
+// ==========================================================================
+
+router.post("/", async (request, response) => {
+  const { r, u, b } = request.body;
+  console.log("request.body: ", request.body);
+
+  try {
+    if (!u || !r || !b) {
+      return response.status(400).send();
+    }
+
+    const book = await booksModel.findById(b);
+    console.log("book", book);
+
+    const user = await usersModel.findById(u);
+    console.log("book", book);
+
+    if (!book || !user) {
+      return response.status(400).send();
+    }
+
+    const new_review = new reviewsModel({
+      review: r,
+      user: u,
+      book: b,
+    });
+
+    const saved_review = await new_review.save();
+
+    book.reviews.push(saved_review);
+    const saved_book = await book.save();
+    if (saved_book) response.json(saved_review);
+    else response.status(400).send();
+  } catch (error) {
+    console.log(error);
+    response.status(400).send();
+  }
+});
+
+// ==========================================================================
+
+router.patch("/:id", async (request, response) => {
+  const { r, u, b } = request.body;
+  console.log("request.body: ", request.body);
+
+  const book = await booksModel.findById(b);
+  const user = await usersModel.findById(u);
+  if (!book || !user) {
+    return response.status(400).send();
+  }
+
+  try {
+    const review = await reviewsModel.findById(request.params.id);
+    if (review) {
+      if (r) review.review = r;
+      if (u) review.user = u;
+      if (b) review.book = b;
+
+      const saved_review = await review.save();
+      response.json(saved_review);
+    } else {
+      response.status(400).send();
+    }
+  } catch (error) {
+    console.log(error);
+    response.status(400).send();
+  }
+});
+
+// ==========================================================================
+
+router.delete("/:id", async (request, response) => {
+  try {
+    const deleted_review = await reviewsModel.findByIdAndDelete(
+      request.params.id
+    );
+
+    if (deleted_review) {
+      const book = await booksModel.findById(deleted_review.book);
+      book.reviews.pull(deleted_review);
+      const updated_book = book.save();
+      if (updated_book) {
+        return response.json(deleted_review);
+      }
+    }
+
+    return response.status(400).send();
+  } catch (error) {
+    console.log(error);
+    response.status(400).send();
+  }
+});
+// ==========================================================================
 
 module.exports = router;
